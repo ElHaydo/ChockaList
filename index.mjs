1ceb20152b16abb995516e6987daf39aa99425f1
Hayden P <petrick.hayden@gmail.com>
2025-09-28 00:05:22 +1000

    Bare bones.

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config(); //NODE_ENV=development
+
+//TODO: Make Application
+console.clear();
+
+class Main{
+
+    static {
+        console.log("--- Calm down skippy! We're not there yet! ---");
+    }
+    
+}
